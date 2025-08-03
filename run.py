77f0ad64c7ae8df41ebea68a0ee3931b26151127
Daeho-Son <thseogh135@gmail.com>
2025-08-04 00:14:26 +0900

    update: `go` 키워드의 절차 간소화

--- run.py
+++ run.py
@@ -7,21 +7,44 @@ def load_urls_from_json(json_file_path):
         return json.load(f)
 
 
-def parse_item(item):
+def parse_item(item, open_browser="", query=""):
+    logo = item if open_browser == "" else open_browser.lower()
+    subtitle = "Type a search keyword and press Enter or just Enter." if open_browser == "" else f"Open/Search in {open_browser}"
     return {
         "uid": item,
         "title": item,
-        "subtitle": "Press Enter to proceed to the next step.",
-        "arg": f"{item} ",
+        "subtitle": subtitle,
+        "arg": json.dumps({
+            "browser": open_browser,
+            "site": item,
+            "query": query
+        }),
         "autocomplete": f"{item} ",
         "icon": {
             "path": f"./logos/{item}.png"
         }
     }
 
+def find_matching_sites(search_term, urls_data):
+    return [site for site in urls_data.keys() if search_term.lower() in site.lower()]
 
-_, query, urls_json_path = sys.argv
-urls_data = load_urls_from_json(urls_json_path)  # JSON 파일 읽기
-items = [parse_item(site) for site in urls_data if query in site]
+site_name = sys.argv[1]
+urls_json_path = sys.argv[-1]
+urls_data = load_urls_from_json(urls_json_path)
+if len(sys.argv) > 3:
+    search_terms = sys.argv[2:-1]
+    query = ' '.join(search_terms)
+else:
+    query = ""
+
+matching_sites = find_matching_sites(site_name, urls_data)
+
+items = []
+for site in matching_sites:
+    items.extend([
+        parse_item(site, "Google Chrome", query),
+        parse_item(site, "Whale", query)
+    ])
+
 
 print(json.dumps({"items": items}))
