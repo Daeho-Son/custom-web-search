0931010db326f079c002540d0db176a1fb9935af
Daeho-Son <thseogh135@gmail.com>
2025-08-06 00:12:24 +0900

    refact: 코드 리팩토링

--- script/run.py
+++ script/run.py
@@ -1,51 +1,60 @@
-import itertools
 import json
 import os
 import sys
 
 
-def parse_item(item, open_browser="", query=""):
-    subtitle = "Type a search keyword and press Enter or just Enter." if open_browser == "" else f"Open/Search in {open_browser}"
-    logo = item if open_browser == "" else open_browser
+def parse_item_exclude_arg(item):
     return {
         "uid": item,
         "title": item,
-        "subtitle": subtitle,
+        "subtitle": "Type a search keyword and press Enter or just Enter.",
+        "autocomplete": item,
+        "icon": {
+            "path": f"./logos/{item}.png"
+        }
+    }
+
+
+def parse_item_include_arg(item, open_browser="", query=""):
+    return {
+        "uid": item,
+        "title": item,
+        "subtitle": f"Open/Search in {open_browser}",
         "arg": json.dumps({
             "site": item,
             "browser": open_browser,
             "query": query
         }),
-        "autocomplete": f"{item + ' '+ query}",
+        "autocomplete": f"{item + ' ' + query}",
         "icon": {
-            "path": f"./logos/{logo}.png"
+            "path": f"./logos/{open_browser}.png"
         }
     }
 
-def find_matched_site_name(keyword, urls_data):
+
+def find_matched_site_names(keyword, urls_data):
     for site_name in urls_data.keys():
         if keyword.lower() in site_name.lower():
-            yield parse_item(site_name)
+            yield parse_item_exclude_arg(site_name)
 
-def get_matched_site(keyword, urls_data):
-    return urls_data.get(keyword)
 
-def ff(site_name, browsers, query):
+def build_alfred_process_items(site_name, browsers, query):
     for browser in browsers:
-        yield parse_item(site_name, browser.strip(), query)
+        yield parse_item_include_arg(site_name, browser.strip(), query)
 
 
 def main():
     keyword = sys.argv[1]
     browsers = os.environ.get('BROWSERS')
     site_url_json = json.loads(os.environ.get('URLS_JSON_CONTENT'))
+
     if len(sys.argv) > 2:
         query = ' '.join(sys.argv[2:])
-        items=list(ff(keyword, browsers.split(','), query))
+        items = list(build_alfred_process_items(keyword, browsers.split(','), query))
     else:
-        items_generate = find_matched_site_name(keyword, site_url_json)
-        items = list(itertools.islice(items_generate, 10))
+        items = list(find_matched_site_names(keyword, site_url_json))
 
     print(json.dumps({"items": items}))
+
 
 main()
