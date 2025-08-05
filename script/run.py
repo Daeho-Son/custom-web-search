166b49a3341c77d5956211adc433ccce4facad88
Daeho-Son <thseogh135@gmail.com>
2025-08-05 22:10:02 +0900

    update: 브라우저를 workflow config에서 입력 받도록 변경

--- script/run.py
+++ script/run.py
@@ -1,6 +1,5 @@
 import json
 import sys
-import os
 
 def load_urls_from_json(json_file_path):
     with open(json_file_path, 'r', encoding='utf-8') as f:
@@ -8,12 +7,10 @@ def load_urls_from_json(json_file_path):
 
 
 def parse_item(item, open_browser="", query=""):
-    logo = item if open_browser == "" else open_browser.lower()
-    subtitle = "Type a search keyword and press Enter or just Enter." if open_browser == "" else f"Open/Search in {open_browser}"
     return {
         "uid": item,
         "title": item,
-        "subtitle": subtitle,
+        "subtitle": "Type a search keyword and press Enter or just Enter." if open_browser == "" else f"Open/Search in {open_browser}",
         "arg": json.dumps({
             "browser": open_browser,
             "site": item,
@@ -28,23 +25,27 @@ def parse_item(item, open_browser="", query=""):
 def find_matching_sites(search_term, urls_data):
     return [site for site in urls_data.keys() if search_term.lower() in site.lower()]
 
-site_name = sys.argv[1]
-urls_json_path = sys.argv[-1]
-urls_data = load_urls_from_json(urls_json_path)
-if len(sys.argv) > 3:
-    search_terms = sys.argv[2:-1]
-    query = ' '.join(search_terms)
-else:
-    query = ""
+def main():
+    site_name = sys.argv[1]
+    browsers = sys.argv[-2]
+    urls_json_path = sys.argv[-1]
+    urls_data = load_urls_from_json(urls_json_path)
+    if len(sys.argv) > 4:
+        search_terms = sys.argv[2:-2]
+        query = ' '.join(search_terms)
+    else:
+        query = ""
 
-matching_sites = find_matching_sites(site_name, urls_data)
+    matching_sites = find_matching_sites(site_name, urls_data)
 
-items = []
-for site in matching_sites:
-    items.extend([
-        parse_item(site, "Google Chrome", query),
-        parse_item(site, "Whale", query)
-    ])
+    items = []
+    for site in matching_sites:
+        for browser in browsers.split(','):
+            items.extend([
+                parse_item(site, browser.strip(), query),
+            ])
 
 
-print(json.dumps({"items": items}))
+    print(json.dumps({"items": items}))
+
+main()
