0cb0b35cbcb86519caf8c2c920cb36c32c2a6263
Daeho-Son <thseogh135@gmail.com>
2025-08-06 00:00:31 +0900

    update: 입력 지연 문제 해결
    [설명]
    입력 지연 문제로 인해 검색어가 누락되는 현상이 발생했고, 이를 해결함
    
    [자세한 설명](https://www.notion.so/sondho/246e01a058b7809b95d0ee7b54356894?source=copy_link)

--- script/run.py
+++ script/run.py
@@ -5,40 +5,47 @@ import sys
 
 
 def parse_item(item, open_browser="", query=""):
+    subtitle = "Type a search keyword and press Enter or just Enter." if open_browser == "" else f"Open/Search in {open_browser}"
+    logo = item if open_browser == "" else open_browser
     return {
         "uid": item,
         "title": item,
-        "subtitle": "Type a search keyword and press Enter or just Enter." if open_browser == "" else f"Open/Search in {open_browser}",
+        "subtitle": subtitle,
         "arg": json.dumps({
-            "browser": open_browser,
             "site": item,
+            "browser": open_browser,
             "query": query
         }),
-        "autocomplete": f"{item} ",
+        "autocomplete": f"{item + ' '+ query}",
         "icon": {
-            "path": f"./logos/{item}.png"
+            "path": f"./logos/{logo}.png"
         }
     }
 
-def find_matching_sites(search_term, urls_data, browsers, query):
-    for site in urls_data.keys():
-        if search_term.lower() in site.lower():
-            for browser in browsers:
-                yield parse_item(site, browser.strip(), query)
+def find_matched_site_name(keyword, urls_data):
+    for site_name in urls_data.keys():
+        if keyword.lower() in site_name.lower():
+            yield parse_item(site_name)
+
+def get_matched_site(keyword, urls_data):
+    return urls_data.get(keyword)
+
+def ff(site_name, browsers, query):
+    for browser in browsers:
+        yield parse_item(site_name, browser.strip(), query)
 
 
 def main():
-    site_name = sys.argv[1]
+    keyword = sys.argv[1]
     browsers = os.environ.get('BROWSERS')
-    urls_data = json.loads(os.environ.get('URLS_JSON_CONTENT'))
+    site_url_json = json.loads(os.environ.get('URLS_JSON_CONTENT'))
     if len(sys.argv) > 2:
-        search_terms = sys.argv[2:]
-        query = ' '.join(search_terms)
+        query = ' '.join(sys.argv[2:])
+        items=list(ff(keyword, browsers.split(','), query))
     else:
-        query = ""
+        items_generate = find_matched_site_name(keyword, site_url_json)
+        items = list(itertools.islice(items_generate, 10))
 
-    items_generate = find_matching_sites(site_name, urls_data, browsers.split(','), query)
-    items = list(itertools.islice(items_generate, 10))
     print(json.dumps({"items": items}))
 
 main()
